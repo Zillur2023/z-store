9d80dec0e5028f72967be3524e98ae0728c935d0
Zillur2023 <zillurrahmanbd12@gmail.com>
2026-07-13 20:32:31 +0600

    update medusa-config.ts and package.json

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -11,6 +11,34 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET,
       cookieSecret: process.env.COOKIE_SECRET,
-    }
-  }
+    },
+     workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+     redisUrl : process.env.REDIS_URL,
+  },
+    admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+  },
+    modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+  ],
 })
